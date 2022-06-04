d5a98013e31bcc29add7dda7bcdc911017423fe8
Fernando Gomez <fernandogomezv@gmail.com>
2022-06-04 19:10:18 +0200

    Deployed review and favorites functionality as well as fixed button positioning in show page of the trail

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,16 @@
 class ReviewsController < ApplicationController
+  def create
+    @review = Review.new(review_params)
+    @trail = Trail.find(params[:trail_id])
+    @user = current_user
+    @review.user = @user
+    @review.trail = @trail
+    @review.save!
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
 end
