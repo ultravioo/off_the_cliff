b3404563e1c93190e7b7be0708da5ee70f74417b
Violette Dunoyer de S <violette.dunoyer@gmail.com>
2022-06-07 17:46:37 +0200

    shox page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :hikes, dependent: :destroy
   has_one_attached :photo
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
