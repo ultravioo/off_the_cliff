825d90aff7a56039d19969959fcc684f2826cade
juan felipe duarte <jf@duartte.co>
2022-06-08 17:44:48 +0200

    button links

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -1,15 +1,13 @@
 import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = ["button"];
+	connect() {
+		console.log(this.buttonTarget);
+	}
 
-  connect() {
-    console.log(this.buttonTarget);
-  }
-
-  menuOnClick(event) {
-    document.getElementById("menu-bar").classList.toggle("change");
-    document.getElementById("nav").classList.toggle("change");
-    document.getElementById("menu-bg").classList.toggle("change-bg");
-  }
+	menuOnClick(event) {
+		document.getElementById("menu-bar").classList.toggle("change");
+		document.getElementById("nav").classList.toggle("change");
+		document.getElementById("menu-bg").classList.toggle("change-bg");
+	}
 }
